a64ebd0bf9c139ef897742cd9d78dfc7df23cf06
Kiwi <chenyujie217@163.com>
2025-04-15 21:03:14 -0400

    Reapply "modify fav and global css"
    This reverts commit 656496bb001c6708ca1cd29ff547226a01f1538e.

--- pages/FavoriteDishesPage.js
+++ pages/FavoriteDishesPage.js
@@ -6,14 +6,15 @@ import { useRouter } from "next/router";
 export default function FavoriteDishesPage() {
     const [dishData, setDishData] = useState([]);
     const router = useRouter();
+    const [dishCount, setDishCount] = useState(0);
 
     const handleViewNutrition = (id, title, image) => {
         router.push(`/NutrientPage?id=${id}&title=${title}&image=${image}`);
-      };
+    };
     
-      const handleViewRecipe = (id, title, image) => {
-        router.push(`/InstructionPage?id=${id}&title=${title}&image=${image}`);
-      };
+    const handleViewRecipe = (id, title, image) => {
+    router.push(`/InstructionPage?id=${id}&title=${title}&image=${image}`);
+    };
 
     useEffect(() => {
         if (typeof window !== "undefined") {
@@ -23,7 +24,7 @@ export default function FavoriteDishesPage() {
 
             }
         }
-    }, []); 
+    }, []);
 
     const removeDish=(removed_id)=>{
         const updatedDishes = dishData.filter(dish => dish.id !== removed_id);
@@ -34,26 +35,32 @@ export default function FavoriteDishesPage() {
         }
     };
 
+    // Load favorite dishes count
+    useEffect(() => {
+        setDishCount(dishData.length);
+    }, [dishData]);
+
     return (
         <div className={styles.favPage}>
             <nav className={styles.navbar}>
                 <Link href={"/HomePage"} className={styles.nav}>Home</Link>
-                <Link href={"/FavoriteDishesPage"} className={styles.nav} scroll={true}>Favorite Dishes</Link>
+                <div className={styles.appName}> WhatToEat?</div> 
+                <Link href={"/FavoriteDishesPage"} className={styles.nav} scroll={true}>My List ({dishCount})</Link>
             </nav>
             <div className={styles.favContent}>
                 <div className={styles.favTitle}>
-                    <h1>Favorite Dishes</h1>
+                    <h2>Favorite Dishes</h2>
                 </div>
                 
-                {dishData.length===0?(<div><p>No saved dishes.</p></div>):
-                (<ul className={styles.listArea}>{dishData.map(dish=>(
+                {dishData.length===0?(<h2 className={styles.favTitle}>No saved dishes.</h2>):
+                 (<ul className={styles.listArea}>{dishData.map(dish=>(
                     <li key={dish.id} className={styles.favList}>
                         <img src={dish.dish_image} alt="dish-img" style={{width: '150px'}} />
                         <h3 className={styles.favTitle}>{dish.dish_title}</h3>
                         <div className={styles.buttonList}>
                             <button onClick={()=>handleViewNutrition(dish.dish_id, dish.dish_title, dish.dish_image)} className={styles.btn}>View Nutrition</button>
-                            <button onClick={()=>handleViewRecipe(dish.dish_id, dish.dish_title, dish.dish_image)} className={styles.btn}>View Instruction</button>
-                            <button onClick={()=>removeDish(dish.id)} className={styles.btn}>Mark as completed</button>
+                            <button onClick={()=>handleViewRecipe(dish.dish_id, dish.dish_title, dish.dish_image)} className={styles.btn}>View Recipe</button>
+                            <button onClick={()=>removeDish(dish.id)} className={styles.btn}>Mark Done</button>
                         </div>
                     </li>   
                 ))}
